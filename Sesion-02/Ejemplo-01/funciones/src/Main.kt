a8951fffb8b47f444a33e84b41ce9ba95f276e6a
Daniel Coutiño <ing.dantonio@gmail.com>
2020-04-23 13:02:02 -0500

    Agregado ejemplo named arguments y default arguments a funciones

--- Sesion-02/Ejemplo-01/funciones/src/Main.kt
+++ Sesion-02/Ejemplo-01/funciones/src/Main.kt
@@ -32,6 +32,22 @@ fun main (){ // en versiones de kotlin anteriores a la 1.3, la función recibe e
     val userValidated = login("Juanito","Navaja")
     println("Usuario loggeado? $userValidated")
 
+
+    //correr area rectangulo con los valores por defecto
+    println("Area con valores por defecto: ${rectangleArea()}")
+
+    //Area de rectangulo con base seteada y altura por defecto
+    println("Area con altura por defecto: ${rectangleArea(10.0)}")
+
+    //Area de rectangulo con por defecto y altura seteada con named arguments
+    println("Area con base por defecto: ${rectangleArea(height = 10.0)}")
+
+    //setear base y altura con named arguments
+    println("Area con datos determinados con named arguments: ${rectangleArea(height = 2.0,base=3.5)}")
+}
+
+fun rectangleArea(base:Double = 20.0,  height: Double = 30.0):Double{
+    return base*height
 }
 
 fun circleArea(radius:Float): Float{
