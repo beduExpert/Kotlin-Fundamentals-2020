89b66eceb7d991ffc6551aef8706cb5b62a25ec1
Daniel Coutiño <ing.dantonio@gmail.com>
2020-04-23 03:32:40 -0500

    correcciones en sesion 2 ejemplo 2

--- Sesion-02/Ejemplo-01/funciones/src/Main.kt
+++ Sesion-02/Ejemplo-01/funciones/src/Main.kt
@@ -14,6 +14,24 @@ fun main (){ // en versiones de kotlin anteriores a la 1.3, la función recibe e
     println(getPi())
 
     printPhi()
+
+    //Funciones locales
+    fun login(user: String, password: String) : Boolean {
+        fun validate(input: String): Boolean{
+            if (input.isEmpty()) {
+                return false
+            }
+            return true
+        }
+        val userValidated = validate(user)
+        val passValidated = validate(password)
+
+        return userValidated && passValidated
+    }
+
+    val userValidated = login("Juanito","Navaja")
+    println("Usuario loggeado? $userValidated")
+
 }
 
 fun circleArea(radius:Float): Float{
